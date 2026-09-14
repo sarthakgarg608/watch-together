8235ddea7f1f87f532011fd434d3fe290c061e13
Sarthak Garg <sarthakgarg608@gmail.com>
2026-09-15 02:58:18 +0530

    Now done with testing moving on the next part connection between frontend and backend

--- backend/src/routes/playback.routes.js
+++ backend/src/routes/playback.routes.js
@@ -1,16 +1,20 @@
 import { Router } from "express";
 
+import authenticate from "../middleware/authMiddleware.js";
+import { requireVerifiedEmail } from "../middleware/authorizationMiddleware.js";
 import {
   getPlaybackState,
   updatePlaybackState,
 } from "../controllers/playback.controller.js";
-
 import {
   requireRoomMember,
   requireRoomHost,
 } from "../middleware/roomMiddleware.js";
 
 const router = Router();
+
+router.use(authenticate);
+router.use(requireVerifiedEmail);
 
 router.get(
   "/:roomCode",
